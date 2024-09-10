1d4d8bf5e3d16dbacb047a852585a6dd87d46324
valaphee <32491319+valaphee@users.noreply.github.com>
2024-09-10 14:29:40 +0200

    Support preproc macros

--- grammar.js
+++ grammar.js
@@ -26,30 +26,49 @@ module.exports = grammar({
     source_file: ($) => $.statements,
 
     statements: ($) =>
-      seq(repeat(seq($.statement, ";")), $.statement, optional(";")),
-    statement: ($) =>
-      choice(
-        $.statement_block,
-        $.nular_expression,
-        $.unary_expression,
-        $.binary_expression,
+      seq(
+        repeat(choice($.preproc, seq($.expression, ";"))),
+        choice($.preproc, seq($.expression, optional(";"))),
       ),
     statement_block: ($) => seq("{", $.statements, "}"),
 
-    nular_expression: ($) =>
-      choice($.identifier, $.number_literal, $.string_literal),
+    expression: ($) =>
+      choice(
+        $.statement_block,
+        $.assignment_expression,
+        $.binary_expression,
+        $.unary_expression,
+        $.parenthesized_expression,
+        $.identifier,
+        $.number,
+        $.string,
+        $.array,
+      ),
+    parenthesized_expression: ($) => seq("(", $.expression, ")"),
+
     identifier: ($) => /[a-zA-Z_][a-zA-Z0-9_]*/,
-    number_literal: ($) => /[-+]?[0-9]*\.?[0-9]+([eE][-+]?[0-9]+)?/,
-    string_literal: ($) =>
+    number: ($) => /[-+]?[0-9]*\.?[0-9]+([eE][-+]?[0-9]+)?/,
+    string: ($) =>
       choice(seq("'", $.string_content, "'"), seq('"', $.string_content, '"')),
     string_content: (_) => token(prec(-1, /([^"`$\\\r\n]|\\(.|\r?\n))+/)),
+    array: ($) => seq("[", commaSep(optional($.expression)), "]"),
+
+    assignment_expression: ($) =>
+      prec.right(
+        PREC.ASSIGNMENT,
+        seq(
+          field("left", $.identifier),
+          field("operator", "="),
+          field("right", $.expression),
+        ),
+      ),
 
     unary_expression: ($) =>
       prec.left(
         PREC.UNARY,
         seq(
           field("operator", choice("+", "-", "!", "not", $.identifier)),
-          field("argument", $.statement),
+          field("argument", $.expression),
         ),
       ),
 
@@ -66,6 +85,7 @@ module.exports = grammar({
         ["<=", PREC.RELATIONAL],
         ["<", PREC.RELATIONAL],
         [$.identifier, PREC.BINARY],
+        ["else", PREC.ELSE],
         ["+", PREC.ADD],
         ["-", PREC.ADD],
         ["min", PREC.ADD],
@@ -82,26 +102,172 @@ module.exports = grammar({
       return choice(
         ...table.map(([operator, precedence]) => {
           return prec.left(
+            // @ts-ignore
             precedence,
             seq(
-              field("left", $.statement),
+              field("left", $.expression),
+              // @ts-ignore
               field("operator", operator),
-              field("right", $.statement),
+              field("right", $.expression),
             ),
           );
         }),
       );
     },
 
-    assignment_expression: ($) =>
-      prec.right(
-        PREC.ASSIGNMENT,
-        seq(
-          field("left", $.identifier),
-          field("operator", "="),
-          field("right", $.statement),
+    preproc: ($) =>
+      choice(
+        $.preproc_if,
+        $.preproc_ifdef,
+        $.preproc_include,
+        $.preproc_def,
+        $.preproc_def_fun,
+        $.preproc_call,
+      ),
+
+    preproc_if: ($) =>
+      seq(
+        "#if",
+        field("condition", $.preproc_expression),
+        "\n",
+        repeat($.expression),
+        field(
+          "alternative",
+          optional(choice($.preproc_else, $.preproc_elif, $.preproc_elifdef)),
+        ),
+        "#endif",
+      ),
+    preproc_ifdef: ($) =>
+      seq(
+        choice("#ifdef", "#ifndef"),
+        field("name", $.identifier),
+        repeat($.expression),
+        field(
+          "alternative",
+          optional(choice($.preproc_else, $.preproc_elif, $.preproc_elifdef)),
+        ),
+        "#endif",
+      ),
+    preproc_else: ($) => seq("#else", repeat($.expression)),
+    preproc_elif: ($) =>
+      seq(
+        "#elif",
+        field("condition", $.preproc_expression),
+        "\n",
+        repeat($.expression),
+        field(
+          "alternative",
+          optional(choice($.preproc_else, $.preproc_elif, $.preproc_elifdef)),
         ),
       ),
+    preproc_elifdef: ($) =>
+      seq(
+        choice("#elifdef", "#elifndef"),
+        field("name", $.identifier),
+        repeat($.expression),
+        field(
+          "alternative",
+          optional(choice($.preproc_else, $.preproc_elif, $.preproc_elifdef)),
+        ),
+      ),
+    preproc_include: ($) =>
+      seq("#include", field("path", $.string), token.immediate(/\r?\n/)),
+    preproc_def: ($) =>
+      seq(
+        "#define",
+        field("name", $.identifier),
+        field("value", optional($.preproc_arg)),
+        token.immediate(/\r?\n/),
+      ),
+    preproc_def_fun: ($) =>
+      seq(
+        "#define",
+        field("name", $.identifier),
+        field("parameters", $.preproc_params),
+        field("value", optional($.preproc_arg)),
+        token.immediate(/\r?\n/),
+      ),
+    preproc_call: ($) =>
+      seq(
+        field("directive", $.preproc_directive),
+        field("argument", optional($.preproc_arg)),
+        token.immediate(/\r?\n/),
+      ),
+
+    preproc_expression: ($) =>
+      choice(
+        $.identifier,
+        alias($.preproc_call_expression, $.call_expression),
+        $.number,
+        $.string,
+        alias($.preproc_unary_expression, $.unary_expression),
+        alias($.preproc_binary_expression, $.binary_expression),
+        alias($.preproc_parenthesized_expression, $.parenthesized_expression),
+      ),
+    preproc_parenthesized_expression: ($) =>
+      seq("(", $.preproc_expression, ")"),
+
+    preproc_params: ($) =>
+      seq(token.immediate("("), commaSep(choice($.identifier, "...")), ")"),
+    preproc_arg: (_) => token(prec(-1, /\S([^/\n]|\/[^*]|\\\r?\n)*/)),
+    preproc_directive: (_) => /#[ \t]*[a-zA-Z0-9]\w*/,
+
+    preproc_call_expression: ($) =>
+      prec(
+        15,
+        seq(
+          field("function", $.identifier),
+          field("arguments", alias($.preproc_argument_list, $.argument_list)),
+        ),
+      ),
+
+    preproc_argument_list: ($) => seq("(", commaSep($.preproc_expression), ")"),
+
+    preproc_unary_expression: ($) =>
+      prec.left(
+        14,
+        seq(
+          field("operator", choice("!", "~", "-", "+")),
+          field("argument", $.preproc_expression),
+        ),
+      ),
+
+    preproc_binary_expression: ($) => {
+      const table = [
+        ["+", 10],
+        ["-", 10],
+        ["*", 11],
+        ["/", 11],
+        ["%", 11],
+        ["||", 1],
+        ["&&", 2],
+        ["|", 3],
+        ["^", 4],
+        ["&", 5],
+        ["==", 6],
+        ["!=", 6],
+        [">", 7],
+        [">=", 7],
+        ["<=", 7],
+        ["<", 7],
+        ["<<", 9],
+        [">>", 9],
+      ];
+
+      return choice(
+        ...table.map(([operator, precedence]) => {
+          return prec.left(
+            precedence,
+            seq(
+              field("left", $.preproc_expression),
+              // @ts-ignore
+              field("operator", operator),
+              field("right", $.preproc_expression),
+            ),
+          );
+        }),
+      );
+    },
 
     comment: (_) =>
       token(
@@ -112,3 +278,11 @@ module.exports = grammar({
       ),
   },
 });
+
+function commaSep1(rule) {
+  return seq(rule, repeat(seq(",", rule)));
+}
+
+function commaSep(rule) {
+  return optional(commaSep1(rule));
+}
--- untitled.sqf
+++ untitled.sqf
@@ -1,14 +1,141 @@
-{test};
-{test};
-{test};
-{test};
-{test};
-test;
-test;
-while { test } do { test };
-if 23 then {
-  test
+/* --------------------------------------------------------------------------------------------------------------------
+	Author:	 	Cre8or
+	Description:
+		[LE]
+		Registers CBA keybindings to the custom gamemode actions.
+
+		Only executed on the client.
+	Arguments:
+		(none)
+	Returns:
+		(nothing)
+-------------------------------------------------------------------------------------------------------------------- */
+
+#include "\a3\editor_f\Data\Scripts\dikCodes.h"
+
+#include "..\..\res\common\macros.inc"
+#include "..\..\res\macros\fnc_initVar.inc"
+
+
+
+
+
+// Target spotting
+private _keyBinds = [
+	[41, [true, false, false]] // Shift + Tilde
+];
+
+// Transfer all keybindings from the original ACE3 pointing function
+if (GVAR(hasMod_ace_finger)) then {
+	private _keyBindsACE = (["ACE3 Common", "ace_finger_finger"] call CBA_fnc_getKeybind) param [8, []];
+	_keyBinds = _keyBinds + _keyBindsACE;
+
+	[
+		MACRO_MISSION_FRAMEWORK_GAMEMODE,
+		QGVAR(kb_spotTarget),
+		"Spot target",
+		{call FUNC(act_spotTarget)},
+		"",
+		_keyBinds,
+		false,
+		0
+	] call FUNC(cba_addKeybindExtended);
+
+	// Discard ACE's original pointing function
+	[
+		"ACE3 Common",
+		"ace_finger_finger",
+		"(Disabled in Conquest)",
+		{false}, // Must be false to overwrite
+		{false}
+	] call CBA_fnc_addKeybind;
+
+} else {
+	[
+		MACRO_MISSION_FRAMEWORK_GAMEMODE,
+		QGVAR(kb_spotTarget),
+		"Spot target",
+		{call FUNC(act_spotTarget)},
+		"",
+		_keyBinds # 0,
+		false,
+		0,
+		false
+	] call CBA_fnc_addKeybind;
 };
 
-for "_i" from 1 to 10 do {
-};
+
+
+// Spawn menu
+[
+	MACRO_MISSION_FRAMEWORK_GAMEMODE,
+	QGVAR(kb_toggleSpawnMenu),
+	"Open/close spawn menu",
+	{call FUNC(act_toggleSpawnMenu)},
+	"",
+	[MACRO_KEYBIND_TOGGLESPAWNMENU, [true, false, false]],
+	false,
+	0,
+	false
+] call CBA_fnc_addKeybind;
+
+
+
+// Role action: resupplying
+[
+	MACRO_MISSION_FRAMEWORK_GAMEMODE,
+	QGVAR(kb_resupplyUnit),
+	"Resupply unit/self",
+	{([player] call FUNC(act_tryResupplyUnit)) param [0, false]},
+	"",
+	[MACRO_KEYBIND_RESUPPLY, [false, false, false]],
+	true,
+	0,
+	false
+] call CBA_fnc_addKeybind;
+
+
+/*
+// Role action: Repairing
+[
+	MACRO_MISSION_FRAMEWORK_GAMEMODE,
+	QGVAR(kb_repairVehicle),
+	"Repair vehicle",
+	{([player] call FUNC(act_tryHealUnit)) param [0, false]},
+	"",
+	[MACRO_KEYBIND_REPAIR, [false, false, false]],
+	true,
+	0,
+	false
+] call CBA_fnc_addKeybind;
+*/
+
+
+// Role action: Healing
+[
+	MACRO_MISSION_FRAMEWORK_GAMEMODE,
+	QGVAR(kb_healUnit),
+	"Heal unit/self",
+	{([player] call FUNC(act_tryHealUnit)) param [0, false]},
+	"",
+	[MACRO_KEYBIND_HEAL, [false, false, false]],
+	true,
+	0,
+	false
+] call CBA_fnc_addKeybind;
+
+
+
+// Give up (unconscious HUD)
+GVAR(kb_act_pressed_giveUp) = false;
+[
+	MACRO_MISSION_FRAMEWORK_GAMEMODE,
+	QGVAR(kb_giveUp),
+	"Give up (unconscious)",
+	{GVAR(kb_act_pressed_giveUp) = true},
+	{GVAR(kb_act_pressed_giveUp) = false},
+	[MACRO_KEYBIND_GIVEUP, [false, false, false]],
+	false,
+	0,
+	false
+] call CBA_fnc_addKeybind;
